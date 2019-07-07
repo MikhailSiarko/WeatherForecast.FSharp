8797cb110388e0c886a5e9dcefb9ea6490401b9e
Mikhail Siarko <serko319@gmail.com>
2019-07-07 17:33:50 +0300

    Fixed user secrets

---
+++ WeatherForecast.FSharp.API/AssemblyInfo.fs
@@ -0,0 +1,7 @@
+module WeatherForecast.FSharp.API.AssemblyInfo
+
+open Microsoft.Extensions.Configuration.UserSecrets
+
+[<assembly: UserSecretsIdAttribute("15ad9009-c662-4e74-921c-c21301f4f9fa")>]
+
+do()
