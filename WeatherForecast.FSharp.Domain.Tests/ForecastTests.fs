257160700c7106c7833913356bb4542f261b437a
Mikhail Siarko <serko319@gmail.com>
2019-08-22 13:04:48 +0300

    Fixed Forecast tests

--- WeatherForecast.FSharp.Domain.Tests/ForecastTests.fs
+++ WeatherForecast.FSharp.Domain.Tests/ForecastTests.fs
@@ -7,11 +7,11 @@ module ForecastTests
     [<Fact>]
     let ``Validate Forecast. Given: Forecast Date = Now - 21 min; ExpirationTime = 20 min. ExpectedResult: Expired`` () =
         match Forecast.validate ({ testForecast1 with Updated = DateTime.UtcNow.AddMinutes(-21.0) }, 20.0) with
-        | Valid -> Assert.False(true)
-        | Expired -> Assert.True(true)
+        | Valid _ -> Assert.False(true)
+        | Expired _ -> Assert.True(true)
         
     [<Fact>]
     let ``Validate Forecast. Given: Forecast Date = Now + 5 min; ExpirationTime = 20 min. ExpectedResult: Valid`` () =
         match Forecast.validate ({ testForecast1 with Updated = DateTime.UtcNow.AddMinutes(5.0) }, 20.0) with
-        | Valid -> Assert.True(true)
-        | Expired -> Assert.False(true)
+        | Valid _ -> Assert.True(true)
+        | Expired _ -> Assert.False(true)
